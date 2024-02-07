bf81eba5aa9cd4fbde53696c939221715b5f325b
Arko <arkoroyb@gmail.com>
2024-02-08 02:31:00 +0600

    project setup and navbar creation

---
+++ src/components/shared/navbar.tsx
@@ -0,0 +1,30 @@
+import Image from 'next/image';
+import logoImg from '@/assets/logo.png'
+import Link from 'next/link';
+
+const Navbar = () => {
+    return (
+        <div className=" min-h-14 max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl mx-auto">
+            <div className="flex justify-between items-center h-full py-5">
+                <div className="">
+                    <Image className='w-[60%]' src={logoImg} width={200} height={200} alt='img' />
+                </div>
+                <div className="">
+                    <ul className="flex gap-8 font-semibold text-[16px]">
+                        <li className="">
+                            <Link href="/">Home</Link>
+                        </li>
+                        <li className="">
+                            <Link href="/">Donation</Link>
+                        </li>
+                        <li className="">
+                            <Link href="/">Statistics</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
