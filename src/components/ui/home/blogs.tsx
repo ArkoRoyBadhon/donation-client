77686e3fc1ec974e473245004a58814cd3e8db10
Arko <arkoroyb@gmail.com>
2024-02-22 13:41:53 +0600

    blog slider responsive

--- src/components/ui/home/blogs.tsx
+++ src/components/ui/home/blogs.tsx
@@ -4,7 +4,8 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { CgArrowLongLeft, CgArrowLongRight } from "react-icons/cg";
-
+import Image from "next/image";
+import imgTest from '@/assets/banner.png'
 
 interface SliderProps {
   dots: boolean;
@@ -36,9 +37,35 @@ function PreviousNextMethods(): ReactElement {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 3,
+    // slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
+    responsive: [
+      {
+        breakpoint: 1440,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
   };
 
   return (
@@ -46,28 +73,39 @@ function PreviousNextMethods(): ReactElement {
       <div className="slider-container relative">
         <Slider ref={sliderRef} {...settings}>
           <div className=" h-[300px]" key={1}>
-            <div className="bg-gray-300 h-full flex justify-center items-center ml-5">
-              <h3 className="">1</h3>
+            <div className="bg-gray-300 h-full ml-5 flex flex-col items-center md:pt-10 pt-0">
+              <div className="px-2 h-[50%] object-contain">
+              <Image className="h-full" src={imgTest} width={500} height={500} alt="" />
+              </div>
+              <h3 className=" mt-5 leading-8 font-semibold px-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, quas.</h3>
             </div>
           </div>
           <div className=" h-[300px]" key={2}>
-            <div className="bg-gray-300 h-full flex justify-center items-center ml-5">
-              <h3 className="">2</h3>
+            <div className="bg-gray-300 h-full ml-5">
+              <div className="px-2 h-[50%] object-contain">
+              <Image className="h-full" src={imgTest} width={500} height={500} alt="" />
+              </div>
+              <h3 className=" mt-5 leading-8 font-semibold px-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, quas.</h3>
+            </div>
+          </div><div className=" h-[300px]" key={3}>
+            <div className="bg-gray-300 h-full ml-5">
+              <div className="px-2 h-[50%] object-contain">
+              <Image className="h-full" src={imgTest} width={500} height={500} alt="" />
+              </div>
+              <h3 className=" mt-5 leading-8 font-semibold px-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, quas.</h3>
+            </div>
+          </div><div className=" h-[300px]" key={4}>
+            <div className="bg-gray-300 h-full ml-5">
+              <div className="px-2 h-[50%] object-contain">
+              <Image className="h-full" src={imgTest} width={500} height={500} alt="" />
+              </div>
+              <h3 className=" mt-5 leading-8 font-semibold px-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, quas.</h3>
             </div>
           </div>
-          <div className=" h-[300px]" key={3}>
-            <div className="bg-gray-300 h-full flex justify-center items-center ml-5">
-              <h3 className="bg-yellow-200">3</h3>
-            </div>
-          </div>
-          <div className=" h-[300px] " key={4}>
-            <div className="bg-gray-300 h-full flex justify-center items-center ml-5">
-              <h3 className="">4</h3>
-            </div>
-          </div>
+
         </Slider>
       </div>
-      <div className="absolute left-[5%] mx-10 w-[85%] md:w-[85%] flex justify-between ">
+      <div className="absolute left-[-5px] md:left-[5%] mx-10 w-[85%] md:w-[85%] flex justify-between ">
         <button className="cursor-pointer" onClick={() => previous()}>
         <CgArrowLongLeft size={30} />
         </button>
--- src/components/ui/home/volunteer.tsx
+++ src/components/ui/home/volunteer.tsx
@@ -1,9 +1,9 @@
 const Volunteer = () => {
   return (
     <div className="h-[400px] bg-black my-10 flex justify-center items-center relative">
-      <div className="border-8 border-white w-[40%] h-[280px] flex justify-center items-center">
-        <div className="absolute w-[60%] bg-yellow-400 h-[45%] rounded-md px-10 py-5">
-          <h4 className="uppercase font-bold text-[24px] text-center text-gray-500">
+      <div className="border-8 border-white w-[60%] md:w-[40%] h-[280px] flex justify-center items-center">
+        <div className="absolute w-[80%] md:w-[60%] bg-yellow-400 h-[55%] md:h-[45%] rounded-md px-3 md:px-10 py-5">
+          <h4 className="uppercase font-bold text-[16px] md:text-[24px] text-center text-gray-500">
             Do you want to work. for the underpriviledged communities and change
             their lives?
           </h4>
